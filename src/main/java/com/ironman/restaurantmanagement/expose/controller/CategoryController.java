8cea439e8d09ffaf053e2ad557b2dda7f544e023
iisaiaso <isaiasibarraonofre@gmail.com>
2024-06-03 19:42:44 -0500

    add Swagger

--- src/main/java/com/ironman/restaurantmanagement/expose/controller/CategoryController.java
+++ src/main/java/com/ironman/restaurantmanagement/expose/controller/CategoryController.java
@@ -3,6 +3,8 @@ package com.ironman.restaurantmanagement.expose.controller;
 import com.ironman.restaurantmanagement.application.service.CategoryService;
 import com.ironman.restaurantmanagement.persistence.entity.Category;
 import com.ironman.restaurantmanagement.persistence.repository.CategoryRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,17 +16,19 @@ import java.util.List;
 @RequestMapping("/categories")
 public class CategoryController {
 
+    @Autowired
    private CategoryService categoryService;
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
 
+    @Operation(summary = "Mostrar todas las categorias")
     @GetMapping
     public List<Category> findAll() {
         return categoryService.findAll();
     }
-
+    @Operation(summary = "Mostrar una categoria por el id")
     @GetMapping("{id}")
     public Category findById(@PathVariable("id") Long id){
         return  categoryService.findById(id);
